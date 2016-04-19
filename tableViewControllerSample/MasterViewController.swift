1bf2f73d1807bd11f9154cdd20201dcb8b550ce0
Sohichiro Nagao <sohnagao@gmail.com>
2016-04-20 00:12:02 +0900

    initial commit

--- tableViewControllerSample/MasterViewController.swift
+++ tableViewControllerSample/MasterViewController.swift
@@ -16,6 +16,7 @@ class MasterViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(#function)
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
 
@@ -30,14 +31,17 @@ class MasterViewController: UITableViewController {
     override func viewWillAppear(animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
         super.viewWillAppear(animated)
+        print(#function)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        print(#function)
     }
 
     func insertNewObject(sender: AnyObject) {
+        print(#function)
         objects.insert(NSDate(), atIndex: 0)
         let indexPath = NSIndexPath(forRow: 0, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
@@ -46,6 +50,7 @@ class MasterViewController: UITableViewController {
     // MARK: - Segues
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        print(#function)
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
                 let object = objects[indexPath.row] as! NSDate
@@ -58,16 +63,50 @@ class MasterViewController: UITableViewController {
     }
 
     // MARK: - Table View
-
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        print(#function)
+        print("indexPath.section = \(indexPath.section)")
+        print("indexPath.row = \(indexPath.row)")
+        let height = UITableViewCell().frame.size.height
+        return height
+    }
+    
+    override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        print(#function)
+        print("indexPath.section = \(indexPath.section)")
+        print("indexPath.row = \(indexPath.row)")
+        let height = UITableViewCell().frame.size.height
+        return height
+    }
+    
+    override func tableView(tableView: UITableView, indentationLevelForRowAtIndexPath indexPath: NSIndexPath) -> Int {
+        print(#function)
+        print("indexPath.row = \(indexPath.row)")
+        return super.tableView(tableView, indentationLevelForRowAtIndexPath: indexPath)
+    }
+    
+//    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+//        print(#function)
+//        super.tableView(tableView, willDisplayCell: cell, forRowAtIndexPath: indexPath)
+//    }
+    
+    
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        print(#function)
         return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(#function)
         return objects.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        print(#function)
+        print("indexPath.row = \(indexPath.row)")
+        print("indexPath.section = \(indexPath.section)")
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
         let object = objects[indexPath.row] as! NSDate
@@ -76,11 +115,13 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        print(#function)
         // Return false if you do not want the specified item to be editable.
         return true
     }
 
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        print(#function)
         if editingStyle == .Delete {
             objects.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
